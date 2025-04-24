325e20865ee035e62706778699391cb0387e2aae
yusufsur <yusuf.sur@sabanciuniv.edu>
2025-04-24 23:12:04 +0300

    import from dilithium pqm4

---
+++ fw/dilithium_fw/symmetric.h
@@ -0,0 +1,31 @@
+#ifndef SYMMETRIC_H
+#define SYMMETRIC_H
+
+#include <stdint.h>
+#include "params.h"
+
+#define SHAKE128_RATE 168
+#define SHAKE256_RATE 136
+#define SHAKE_PAD 0x1F
+#define STREAM128_BLOCKBYTES SHAKE128_RATE
+#define STREAM256_BLOCKBYTES SHAKE256_RATE
+
+typedef void stream128_state;
+typedef void stream256_state;
+
+#define dilithium_shake128_stream_init DILITHIUM_NAMESPACE(dilithium_shake128_stream_init)
+void dilithium_shake128_stream_init(const uint8_t seed[SEEDBYTES], uint16_t nonce);
+
+#define dilithium_shake256_stream_init DILITHIUM_NAMESPACE(dilithium_shake256_stream_init)
+void dilithium_shake256_stream_init(const uint8_t seed[CRHBYTES], uint16_t nonce);
+
+#define stream128_init(SEED, NONCE) dilithium_shake128_stream_init(SEED, NONCE)
+#define stream256_init(SEED, NONCE) dilithium_shake256_stream_init(SEED, NONCE)
+
+#define stream128_squeezeblocks(OUT, OUTBLOCKS) \
+        keccak_squeeze((uint32_t*)OUT, NULL, (OUTBLOCKS)*(SHAKE128_RATE/4))
+
+#define stream256_squeezeblocks(OUT, OUTBLOCKS) \
+        keccak_squeeze((uint32_t*)OUT, NULL, (OUTBLOCKS)*(SHAKE256_RATE/4))
+
+#endif
