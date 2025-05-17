78337a9ebc489b120e10fbe7627dd1311cf99730
yusufsur <yusuf.sur@sabanciuniv.edu>
2025-05-17 17:58:54 +0300

    change randombyte gen cuz linux do not support prev version

--- fw/dilithium_fw/randombytes.h
+++ fw/dilithium_fw/randombytes.h
@@ -2,12 +2,12 @@
 #define RANDOMBYTES_H
 #include <stdint.h>
 
-#ifdef _WIN32
-#include <crtdefs.h>
-#else
-#include <unistd.h>
-#endif
 
-int randombytes(uint8_t *buf, size_t n);
+#include "stdint.h"
+
+
+int randombytes(unsigned char *x, unsigned long long xlen);
+
+uint16_t rand16();
 
 #endif
