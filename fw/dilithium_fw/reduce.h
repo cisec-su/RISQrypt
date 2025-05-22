e4ef9672b9ffe859d6a29f18dd994ba81b164a1c
yusufsur <yusuf.sur@sabanciuniv.edu>
2025-05-22 19:13:06 +0300

    reduce header

--- fw/dilithium_fw/reduce.h
+++ fw/dilithium_fw/reduce.h
@@ -8,22 +8,15 @@
 #define QINV 58728449 // q^(-1) mod 2^32
 
 #define montgomery_reduce DILITHIUM_NAMESPACE(montgomery_reduce)
-/*************************************************
-* Name:        montgomery_reduce
-*
-* Description: For finite field element a with -2^{31}Q <= a <= Q*2^31,
-*              compute r \equiv a*2^{-32} (mod Q) such that -Q < r < Q.
-*
-* Arguments:   - int64_t: finite field element a
-*
-* Returns r.
-**************************************************/
-static inline int32_t montgomery_reduce(int64_t a) {
-  int32_t t;
+int32_t montgomery_reduce(int64_t a);
 
-  t = (int64_t)(int32_t)a*QINV;
-  t = (a - (int64_t)t*Q) >> 32;
-  return t;
-}
+#define reduce32 DILITHIUM_NAMESPACE(reduce32)
+int32_t reduce32(int32_t a);
+
+#define caddq DILITHIUM_NAMESPACE(caddq)
+int32_t caddq(int32_t a);
+
+#define freeze DILITHIUM_NAMESPACE(freeze)
+int32_t freeze(int32_t a);
 
 #endif
