8dd99cff7ebeec165e4d635bba67c412b64c36de
yusufsur <yusuf.sur@sabanciuniv.edu>
2025-05-22 19:14:22 +0300

    message change main

--- fw/dilithium_fw/main.c
+++ fw/dilithium_fw/main.c
@@ -21,7 +21,7 @@ int main() {
     uint8_t pk[CRYPTO_PUBLICKEYBYTES];
     uint8_t sk[CRYPTO_SECRETKEYBYTES];
     uint8_t sig[CRYPTO_BYTES];
-    uint8_t message[] = "Test message for Dilithium signing";
+    uint8_t message[] = "Test message for Dilithium3";
     size_t msg_len = sizeof(message) - 1;
     size_t sig_len;
     int ret;
