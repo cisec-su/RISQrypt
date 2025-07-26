7c066fc118686488f3947d8bd0f966ab29f3994b
yusufsur <yusuf.sur@sabanciuniv.edu>
2025-07-26 14:54:29 +0300

    falcon init commit

---
+++ fw/falcon_hw/randombytes.h
@@ -0,0 +1,13 @@
+#ifndef RANDOMBYTES_H
+#define RANDOMBYTES_H
+#include <stdint.h>
+
+
+#include "stdint.h"
+
+
+int randombytes(unsigned char *x, unsigned long long xlen);
+
+uint16_t rand16();
+
+#endif
