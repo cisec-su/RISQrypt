331dccf3bf7b61c9d987f915e85a737e35430782
Tolun Tosun <toluntosun@sabanciuniv.edu>
2025-10-18 16:50:51 +0300

    trace collection succeed

---
+++ sca/fw/examples/simpleserial_example/simpleserial_example.c
@@ -0,0 +1,97 @@
+/*
+    This file is part of the ChipWhisperer Example Targets
+    Copyright (C) 2012-2017 NewAE Technology Inc.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdint.h>
+#include <stdlib.h>
+#include "util.h"
+#include "cw305.h"
+#include "simpleserial.h"
+
+
+#define VERBOSE
+
+uint8_t key[16];
+volatile uint8_t ct[16];
+
+
+uint8_t get_key(uint8_t* k, uint8_t len)
+{
+#ifdef VERBOSE
+    print_string("SimpleSerial::get_key command received\n");
+#endif
+
+	for (uint8_t i = 0; i < 16; i++) {
+		key[i] = k[i];
+	}
+
+#ifdef VERBOSE
+    print_string("Key set to: ");
+    print_hex(k, len, 0);
+    print_string("\n");
+#endif
+	return 0x00;
+}
+
+uint8_t get_pt(uint8_t* pt, uint8_t len)
+{
+#ifdef VERBOSE
+    print_string("SimpleSerial::get_pt command received\n");
+    print_string("pt: ");
+    print_hex(pt, len, 0);
+    print_string("\n");
+#endif
+
+    cw305_trigger_up();
+
+
+	for (uint8_t i = 0; i < 16; i++) {
+		ct[i] = pt[i] + key[i];
+		ct[i] = 0;
+		ct[i] = pt[i] + key[i];
+		ct[i] = 0;
+		ct[i] = pt[i] + key[i];
+    }
+
+    cw305_done_set_trigger_down();
+    simpleserial_put('r', 16, (uint8_t*) pt);
+
+    return 0x00;
+}
+
+
+int main(void)
+{
+    cw305_trigger_down();
+    print_string("SimpleSerial Example\n");
+
+    /* Uncomment this to get a HELLO message for debug */
+
+    // putch('h');
+    // putch('e');
+    // putch('l');
+    // putch('l');
+    // putch('o');
+    // putch('\n');
+
+	simpleserial_init();
+    simpleserial_addcmd('p', 16, get_pt);
+    simpleserial_addcmd('k', 16, get_key);
+
+    while(1)
+        simpleserial_get();
+}
---
+++ sca/host/CW305_rq.py
@@ -0,0 +1,103 @@
+from chipwhisperer.capture.targets._base import TargetTemplate
+from chipwhisperer.hardware.naeusb.naeusb import packuint32
+
+from chipwhisperer.capture.targets.CW305 import CW305
+from chipwhisperer.capture.targets.SimpleSerial import SimpleSerial
+
+
+from chipwhisperer.logging import *
+
+
+class _DummySer():
+    def flush(self):
+        pass
+
+
+class CW305_rq(CW305, SimpleSerial):
+
+    """CW305 Target for RISQrypt Firmware.
+       Communicates over SimpleSerial protocol over CW305 USB Interface.
+    """
+    def __init__(self):
+        TargetTemplate.__init__(self)
+
+        #NOTE: default values to make linter happy - these are never correct
+        self.REG_DATA = 0x0
+        self.REG_DONE = 0x1
+
+        self._naeusb = None
+        self.pll = None
+        self.fpga = None
+        self.ss2 = None
+        self.platform = None
+
+        self.ser = _DummySer()
+
+        self.hw = None
+        self.oa = None
+
+        self._woffset_sam3U = 0x000
+        self.registers = 2 # number of registers we expect to find
+        self.bytecount_size = 7 # pBYTECNT_SIZE in Verilog
+
+        self._clksleeptime = 1
+        self._clkusbautooff = True
+        self.check_done = False
+        self.last_key = bytearray([0]*16)
+        self.target_name = 'CW305_RISQrypt'
+
+    def slurp_defines(self, defines_files=None):
+        raise NotImplementedError(f"SLURP is not implemented for {self.target_name} target")
+
+    def _reg_not_exist_str(reg_name):
+        return reg_name + f" Register doesn't exist in {self.target_name} target"
+
+    @property
+    def crypt_type(self):
+        raise NotImplementedError(self._reg_not_exist_str("CRYPT_TYPE"))
+
+    @property
+    def crypt_rev(self):
+        raise NotImplementedError(self._reg_not_exist_str("CRYPT_REV"))        
+
+    @property
+    def core_type(self):
+        raise NotImplementedError(self._reg_not_exist_str("CORE_TYPE"))    
+
+    def loadEncryptionKey(self, key):
+        raise NotImplementedError(self._reg_not_exist_str("CRYPT_KEY")) 
+
+    def loadInput(self, inputtext):
+        raise NotImplementedError(self._reg_not_exist_str("CRYPT_TEXTIN"))
+
+    def is_done(self):
+        """Check if FPGA is done"""
+        if self.check_done:
+            while(True):
+                status = self.fpga_read(1, 1)
+                if status[0] == 0xff:
+                    break
+            return True
+        else:
+            return True
+
+    def readOutput(self):
+        raise NotImplementedError(self._reg_not_exist_str("CRYPT_TEXTOUT"))
+
+    def go(self):
+        raise NotImplementedError(self._reg_not_exist_str("CRYPT_GO"))
+
+    def _con(self, *args, slurp=False, **kwargs):
+        CW305._con(self, *args, slurp=slurp, **kwargs)
+
+    def write(self, data, timeout=0):
+        self.fpga_write(self.REG_DATA, data)
+
+    def read(self, num_char = 0, timeout=250):
+        return self.fpga_read(self.REG_DATA, num_char).decode()
+
+    def simpleserial_write(self, cmd, data, end='\n'):
+        SimpleSerial.simpleserial_write(self, cmd, data, end=end)
+
+    def simpleserial_read(self, cmd, pay_len, end='\n', timeout=250, ack=True):
+        return SimpleSerial.simpleserial_read(self, cmd, pay_len, end=end, ack=ack)
