51057f02e1cda8581ab90fab7716d47fb41c1466
Tolun Tosun <toluntosun@sabanciuniv.edu>
2026-02-03 10:50:42 +0300

    num dma accs iincreased for x2x

--- processor/fpga_uart/fpga_top.sv
+++ processor/fpga_uart/fpga_top.sv
@@ -1,3 +1,16 @@
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+//////                                                                            ////// 
+//////   //////   //    ////     ////                                    //       //////
+//////   //   //  //   //       //  //      /////   //    //   //////    //       //////
+//////   /////    //     ///   //    //    //       //    //   //   //   ////     //////
+//////   //  //   //       //   //  ///    //       //    //   //   //   //       //////
+//////   //   //  //   /////     //// //   //        //////    /////      ////    //////
+//////                                                    //   //                 //////
+//////                                              ///////    //                 //////
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+
 module fpga_top
     (
         input                       clk_i  ,
@@ -34,7 +47,7 @@ parameter USB_ADDR_WIDTH   = 21      ;
 parameter CW305_FIFO_BSIZE = 128     ;
 parameter PRNG_OFF_EN      = 1       ;
 `else
-parameter SYS_CLK_FREQ   = 59000000;
+parameter SYS_CLK_FREQ   = 58000000;
 parameter PRNG_OFF_EN    = 0       ;
 `endif
 parameter UART_BAUD      = 115200  ;
@@ -47,7 +60,7 @@ parameter RAM_INST_START = 32'h0000_0820;
 parameter RAM_INST_END   = 32'h0000_FFFF;
 
 parameter RAM_DATA_START = 32'h0001_0000;
-parameter RAM_DATA_END   = 32'h0002_FFFF;
+parameter RAM_DATA_END   = 32'h0003_FFFF;
 
 parameter MTIME_START    = 32'h2000_8000;
 parameter MTIME_END      = 32'h2000_800F;
@@ -77,14 +90,15 @@ parameter CW305_START    = 32'h1004_1000;
 parameter CW305_END      = 32'h1004_1003;
 
 
-localparam NUM_DMA_ACCS  = (MODE == 2)? 3 : (MODE == 1)? 2 : 0;
+localparam NUM_ACCS      = (MODE == 2)? 3 : (MODE == 1)? 2 : 0;
+localparam NUM_DMA_ACCS  = (MODE == 2)? 4 : (MODE == 1)? 2 : 0;
 localparam NUM_DMA_ACCS_ = (NUM_DMA_ACCS == 0) ? 1 : NUM_DMA_ACCS; // to avoid zero-width arrays
 `ifdef CW305
 localparam CW305_SLAVE   = 1;
 `else
 localparam CW305_SLAVE   = 0;
 `endif
-localparam NUM_SLAVES    = 6 + NUM_DMA_ACCS + CW305_SLAVE;
+localparam NUM_SLAVES    = 6 + NUM_ACCS + CW305_SLAVE;
 
 
 wire clk;
@@ -547,7 +561,6 @@ end
 
 
 if (MODE == 2) begin
-
 x2x_acc_top #(
     .BASE_ADDR  (X2X_START  ),
     .PRNG_OFF_EN(PRNG_OFF_EN)
@@ -565,17 +578,17 @@ x2x_acc_top #(
     .wb_rst_i  (wb_rst_i  [8]),
     .wb_clk_i  (wb_clk_i  [8]),
     
-    .dma_cyc_i  (dma_cyc_i  [2]),
-    .dma_stb_i  (dma_stb_i  [2]),
-    .dma_we_i   (dma_we_i   [2]),
-    .dma_adr_i  (dma_adr_i  [2]),
-    .dma_dat_i  (dma_dat_i  [2]),
-    .dma_sel_i  (dma_sel_i  [2]),
-    .dma_stall_o(dma_stall_o[2]),
-    .dma_ack_o  (dma_ack_o  [2]),
-    .dma_dat_o  (dma_dat_o  [2]),
-    .dma_err_o  (dma_err_o  [2]),
-    .dma_rst_i  (dma_rst_i  [2])
+    .dma_cyc_i  (dma_cyc_i  [3:2]),
+    .dma_stb_i  (dma_stb_i  [3:2]),
+    .dma_we_i   (dma_we_i   [3:2]),
+    .dma_adr_i  (dma_adr_i  [3:2]),
+    .dma_dat_i  (dma_dat_i  [3:2]),
+    .dma_sel_i  (dma_sel_i  [3:2]),
+    .dma_stall_o(dma_stall_o[3:2]),
+    .dma_ack_o  (dma_ack_o  [3:2]),
+    .dma_dat_o  (dma_dat_o  [3:2]),
+    .dma_err_o  (dma_err_o  [3:2]),
+    .dma_rst_i  (dma_rst_i  [3:2])
 );
 
 end
