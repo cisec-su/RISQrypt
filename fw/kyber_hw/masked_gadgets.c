7f7d332d07bec8caf09a2cba9ab97554b7c68db1
Tolun Tosun <toluntosun@sabanciuniv.edu>
2026-02-16 15:37:49 +0300

    ttest conf added to masked_gadgets

--- fw/kyber_hw/masked_gadgets.c
+++ fw/kyber_hw/masked_gadgets.c
@@ -109,11 +109,13 @@ int masked_gadgets_zero_test_mul(masked_u32 a) {
     uint32_t *src;
 
     masked_gadgets_init_q_carrier();
+#ifndef TTEST
     x2x_prng_read_nonzero(rng_buffer, buffer_len);
-    // x2x_prng_read(rng_buffer, buffer_len);
-    // for (j = 0; j < buffer_len; j++) {
-    //     rng_buffer[j] = 1;
-    // }
+#else
+    for (j = 0; j < buffer_len; j++) {
+        rng_buffer[j] = 1;
+    }
+#endif
 
     ntt_lite_set_ctrl(1, 32, NTT_LITE_MODE_SINGLE);
 
